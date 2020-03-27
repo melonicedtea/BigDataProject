e5793c5e43d0b95068243e64c38b3f76165719bf
Ido Hiemstra <ido.hiemstra@student.nhlstenden.com>
2020-03-27 15:32:47 +0100

    cleanup in functions

--- tsv_to_csv_parser_v0.2/src/main/java/com/mycompany/parser/Data.java
+++ tsv_to_csv_parser_v0.2/src/main/java/com/mycompany/parser/Data.java
@@ -3,12 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.parser;
 
 /**
  *
  * @author darkblade
  */
+
+package com.mycompany.parser;
+
+
 public class Data{
     
     String line;
--- tsv_to_csv_parser_v0.2/src/main/java/com/mycompany/parser/Main.java
+++ tsv_to_csv_parser_v0.2/src/main/java/com/mycompany/parser/Main.java
@@ -27,44 +27,16 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-        List<String> lines = new ArrayList<>();
         String fileToParse = "resources/new.tsv";
         String fileParsed = "resources/dataParsed.csv";
-        String pattern = "(.*)";
-        String delimiter = "|";
-        int groupCount = 1;
 
         try {
             File myObj = new File(fileToParse);
             try (Scanner myReader = new Scanner(myObj)) {
                 String firstLine = myReader.nextLine();
-                for(int i = 0; i < firstLine.length(); i++){
-                    if(firstLine.charAt(i) == '\t'){
-                        groupCount++;
-                    }
-                }
-                for(int j = 1; j < groupCount; j++){
-                    pattern = "(.*?)\t" + pattern;
-                }
+                String pattern = createPattern(firstLine);
                 Pattern titlePattern = Pattern.compile(pattern);
-
-                while (myReader.hasNextLine()) {
-                    String data = myReader.nextLine();
-                    String line = "";
-                    Matcher m = titlePattern.matcher(data);
-                    if (m.find()) {
-                        for(int i = 1; i < m.groupCount(); i++){
-                            line = line + m.group(i) + delimiter;
-                        }
-                        line = line + m.group(m.groupCount());
-                    }
-                    else {
-                        System.out.println("no match");
-                    }
-
-                    lines.add(line);
-                }
+                List<String> lines = getLines(myReader, titlePattern);
                 Path file = Paths.get(fileParsed);
                 Files.write(file, lines);
             }
@@ -74,5 +46,40 @@ public class Main {
         }
     }
 
+    public static String createPattern(String firstLine) {
+        String pattern = "(.*)";
+        int groupCount = 1;
+        for (int i = 0; i < firstLine.length(); i++) {
+            if (firstLine.charAt(i) == '\t') {
+                groupCount++;
+            }
+        }
+        for (int j = 1; j < groupCount; j++) {
+            pattern = "(.*?)\t" + pattern;
+        }
+        return pattern;
+    }
+
+    public static List<String> getLines(Scanner myReader, Pattern titlePattern) {
+        String delimiter = "|";
+        List<String> lines = new ArrayList<>();
+
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            String line = "";
+            Matcher m = titlePattern.matcher(data);
+            if (m.find()) {
+                for (int i = 1; i < m.groupCount(); i++) {
+                    line = line + m.group(i) + delimiter;
+                }
+                line = line + m.group(m.groupCount());
+            } else {
+                System.out.println("no match");
+            }
+
+            lines.add(line);
+        }
+        return lines;
+    }
 
 }
